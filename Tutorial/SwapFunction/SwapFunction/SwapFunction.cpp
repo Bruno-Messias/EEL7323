0c00939a092434008fe924a5601d1a1d6198da6a
Bruno Cayres Messias <brunocmessias@hotmail.com>
2020-12-21 22:43:52 -0300

    trying to add generic function

--- Tutorial/SwapFunction/SwapFunction/SwapFunction.cpp
+++ Tutorial/SwapFunction/SwapFunction/SwapFunction.cpp
@@ -4,39 +4,56 @@
 */
 
 #include <iostream>
+#include <complex>
 
-using namespace std;
+//using namespace std;
 
-inline void swap(int& i, int& j) //inline is more efficint
-//! call by reference
-// int& -> signature of the arguments
-{
-    int temp = i; 
-    i = j;
-    j = temp;
-}
-
-inline void swap(double& i, double& j)//! only use inline for small function
-{
-    double temp = i;
-    i = j;
-    j = temp;
-}
+//inline void swap(int& i, int& j) //inline is more efficint
+////! call by reference
+//// int& -> signature of the arguments
+//{
+//    int temp = i; 
+//    i = j;
+//    j = temp;
+//}
+//
+//inline void swap(double& i, double& j)//! only use inline for small function
+//{
+//    double temp = i;
+//    i = j;
+//    j = temp;
+//}
 
 //! C++ has overloading(can have two function with the same name without problems) and call by reference
+
+//! Adding template(generic function)
+
+template <class T>//! capitalized T is a convencion (meta variable)
+inline void swap(T& d, T& s)
+{
+    T temp = d;
+    d = s;
+    s = temp;
+}
 
 int main()
 {
     int m = 5, n = 10;
     double x = 5.3, y = 10.6;
 
-    cout << "inputs: " << m << "," << n << endl;
-    swap(m, n);
-    cout << "outputs: " << m << "," << n << endl;
+    std::complex<double> r(2.4, 3.5), s(3.4, 6.7);
 
-    cout << "double inputs: " << x << "," << y << endl;
-    swap(x, y); //! C understand the signature name of the type of the variable
-    cout << "double outputs: " << x << "," << y << endl;
+    std::cout << "inputs: " << m << "," << n << std::endl;
+    ::swap(m, n);
+    std::cout << "outputs: " << m << "," << n << std::endl;
+
+    std::cout << "double inputs: " << x << "," << y << std::endl;
+    ::swap(x, y);
+    std::cout << "double outputs: " << x << "," << y << std::endl;
+
+    std::cout << "complex inputs: " << r << "," << s << std::endl;
+    ::swap(r, s);
+    std::cout << "complex outputs: " << r << "," << s << std::endl;
 
     return 0;
 }
