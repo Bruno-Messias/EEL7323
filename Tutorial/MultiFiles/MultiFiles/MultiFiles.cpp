8e628be9216d86576ecff1388f2b3111245b9ceb
Bruno Cayres Messias <brunocmessias@hotmail.com>
2020-12-25 19:37:35 -0300

    Adding multi files compilation example with default value assigment

---
+++ Tutorial/MultiFiles/MultiFiles/MultiFiles.cpp
@@ -0,0 +1,21 @@
+/* MultiFiles.cpp : Implement multi file compilation
+*  Date: 25/12/2020
+*  Author: Bruno C. Messias
+*/
+#include <iostream>
+#include "utils.h"
+
+int main()
+{
+    Retangle ret;
+    ret.length = 10;
+    ret.width = 10;
+
+    std::cout << area(ret.length, ret.width) << "\n";
+    std::cout << area(ret) << "\n";
+    std::cout << pow(3,3) << "\n";
+    std::cout << pow(3) << "\n";
+
+    
+    return 0;
+}
---
+++ Tutorial/MultiFiles/MultiFiles/utils.cpp
@@ -0,0 +1,23 @@
+//Definition of utils.cpp
+
+#include "utils.h"
+
+double area(double length, double width)
+{
+    return length * width;
+}
+
+double area(Retangle ret)
+{
+    return ret.length * ret.width;
+}
+
+double pow(double base, int pow)
+{
+    double total = 1;
+    for (int i = 0; i < pow; i++)
+    {
+        total *= base;
+    }
+    return total;
+}
