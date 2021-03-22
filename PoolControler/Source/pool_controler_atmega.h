4d90104c790e0f97c6d9db869b8798863c33b95d
Bruno Cayres Messias <brunocmessias@hotmail.com>
2021-03-22 18:32:39 -0300

    creating final Source

---
+++ PoolControler/Source/pool_controler_atmega.h
@@ -0,0 +1,36 @@
+#ifndef POOL_CONTROLER_ATMEGA_H
+#define POOL_CONTROLER_ATMEGA_H
+
+// -- Frequency --
+#define F_CPU 16000000 // frequency oscillator
+
+#include <avr/io.h>
+#include <util/delay.h>
+#include <avr/interrupt.h>
+
+#include "pins_atmega328p.h"
+#include "pool_controler.cpp"
+#include "timer.cpp"
+#include "MAX7219_driver.cpp"
+
+
+class PoolControlerAtmega : public PoolControler
+{
+	Timer time;
+	MaxDriver led;
+	int estate, value1, value2, value;
+	bool timeout, pump, heater, reset, sw, low;
+public:
+	PoolControlerAtmega();
+	~PoolControlerAtmega() { }
+	virtual void inputSW();
+	virtual void FSM();
+	virtual void Inputs();
+	virtual void Outputs();
+	void setEstate(int);
+	void displayTimer();
+};
+
+#endif // !POOL_CONTROLER_ATMEGA_H
+
+
---
+++ PoolControler/Source/pool_controler_windowns.h
@@ -0,0 +1,30 @@
+#ifndef POOL_CONTROLER_WINDOWNS_H
+#define POOL_CONTROLER_WINDOWNS_H
+
+#include "pool_controler.cpp"
+#include "timer.cpp"
+
+#include <windows.h>
+#include <conio.h>
+#include <stdlib.h>
+#include <iostream>
+
+#define sec 1000
+#define minute 60000
+
+class PoolControlerWindowns: public PoolControler
+{
+	Timer time;
+	bool timeout, pump, heater, reset, sw, low;
+	int estate, insertSW, insertReset, minuteTimer;
+public:
+	PoolControlerWindowns();
+	void inputSW() override;
+	void FSM() override;
+	void Inputs() override;
+	void Outputs() override;
+	void displayTimer();
+	void setEstate(int);
+};
+
+#endif // !POOL_CONTROLER_WINDOWNS_H
