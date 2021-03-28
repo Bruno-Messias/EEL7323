6f2e764c3d433179dc7386348b37d14ca292aaec
Bruno Cayres Messias <brunocmessias@hotmail.com>
2021-03-28 18:21:37 -0300

    adding clock calenadr and adding list of todo's

---
+++ PoolControler/PoolControler_AVR/PoolControler_AVR/PoolControler_AVR/ClockCalendar.cpp
@@ -0,0 +1,58 @@
+﻿
+#include "ClockCalendar.h"
+
+Clock::Clock (int h, int m, int s, int pm){
+	hr = h;
+	min = m;
+	sec = s;
+	isPm = pm;
+}
+void Clock::setClock (int h, int m, int s, int pm){
+	hr = h;
+	min = m;
+	sec = s;
+	isPm = pm;
+}
+void Clock::readClock (int& h, int& m, int& s, int& pm){
+	h = hr;
+	m = min;
+	s = sec;
+	pm = isPm;
+}
+void Clock::advance (){
+	if (sec < 59)
+	sec++;
+	else {
+		sec = 0;
+		if (min < 59)
+		min++;
+		else {
+			min = 0;
+			if (hr < 12)
+			hr++;
+			else {
+				hr = 0;
+			}
+		}
+	}
+}
+
+
+
+Calendar::Calendar (int m, int d, int y){};
+
+void Calendar::setCalendar (int m, int d, int y){};
+
+void Calendar::readCalendar (int& m, int& d, int& y){};
+
+void Calendar::advance (){};
+
+ClockCalendar::ClockCalendar (int mt, int d, int y, int h, int m, int s, int pm) : Clock (h, m, s, pm), Calendar (mt, d, y){
+}
+
+void ClockCalendar::advance (){ // avancar o calendario, caso o clock
+	int wasPm = isPm;       // mude de AM para PM.
+	Clock::advance();
+	if (wasPm && !isPm)
+	Calendar::advance();
+}
--- PoolControler/PoolControler_AVR/PoolControler_AVR/PoolControler_AVR/main.cpp
+++ PoolControler/PoolControler_AVR/PoolControler_AVR/PoolControler_AVR/main.cpp
@@ -10,6 +10,27 @@ bool flag = false; //flag interrupt, global variable
 //#include "pool_controler_windowns.cpp"
 #include "pool_controler_atmega.cpp"
 
+/*
+TODO: -- Implementar logs --
+	Sistema desligado por timeout
+	Botão acionando, mas sem energia
+	Botão acionado antes de timeout
+
+FILA de dados
+Deve conter ID do Controlador e Data/hora do log
+
+Funções do LOG:
+	Listar todos os eventos ocorridos
+	Informar tempo total que o o aqucedor estve ligado em um intervalo de data
+	Listar período de maior utilização do sistema
+	
+Objetivos:
+Envio via UART
+Mandar a Fila do Atmega e ser esvaziadaa -> estrutura no computador armazenar em lista esses dados(armazenar em .log no pc)
+*/
+
+
+
 int main(void)
 {
 	//PoolControlerWindowns Controler;
