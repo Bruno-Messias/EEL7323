ab03170e02b705260e0376bf34a3af92dfda1cee
Bruno Cayres Messias <brunocmessias@hotmail.com>
2021-03-31 20:40:57 -0300

    Add changes for structures of data for tests

--- PoolControler/PoolControler/PoolControler/Node.h
+++ PoolControler/PoolControler/PoolControler/Node.h
@@ -1,30 +1,12 @@
-/*
-   File Node.h
 
-   Class Node
-
-   Class Node stores integer values for a linked list.
-
-   This file has the Node's interface (header file).
-
-   Eduardo Augusto Bezerra <eduardo.bezerra@ufsc.br>
-   Departamento de Engenharia Eletrica
-
-   Data da criacao: Abril de 2006.
-   Data da ultima alteracao: 8 de outubro de 2015.
-
-*/
+#include <string.h>
 
 class Node {
-
-    int val;
+    std::string val;
     Node* next;
-
 public:
-
-    Node(int dat, Node* nxt);
-    int getVal();
+    Node(std::string val, Node* nxt);
+    std::string getVal();
     Node* getNext();
-    void setVal(int dat);
     void setNext(Node* nxt);
 };
--- PoolControler/PoolControler/PoolControler/PoolControler.cpp
+++ PoolControler/PoolControler/PoolControler/PoolControler.cpp
@@ -3,32 +3,47 @@
 
 #include <iostream>
 
+using namespace std;
+
 bool flag = false; //flag interrupt, global variable
 
-#include "pool_controler_windowns.cpp"
+//#include "pool_controler_windowns.cpp"
+
+#include "List.cpp"
 
 //-- Uncomment bellow for Atmega Interface: 
 //#include "pool_controler_atmega.cpp"
 
 int main(void)
 {
-	PoolControlerWindowns Controler;
-	//-- Uncomment bellow for Atmega Interface:
-	//PoolControlerAtmega Controler;
+	//PoolControlerWindowns Controler;
+	////-- Uncomment bellow for Atmega Interface:
+	////PoolControlerAtmega Controler;
 
-	Controler.inputSW();
+	//Controler.inputSW();
 
-	while (1)
-	{
-		Controler.Inputs();
-		Controler.Outputs();
-		Controler.FSM();
-		if (flag)
-		{
-			Controler.setEstate(1);  //If interrupt occur the next estate is INIT to reset the timer
-			flag = false;
-		}
-	}
+	//while (1)
+	//{
+	//	Controler.Inputs();
+	//	Controler.Outputs();
+	//	Controler.FSM();
+	//	if (flag)
+	//	{
+	//		Controler.setEstate(1);  //If interrupt occur the next estate is INIT to reset the timer
+	//		flag = false;
+	//	}
+	//}
+
+	List lista1;
+
+	lista1.insertBeforeFirst("helllo");
+	lista1.insertAfterLast("world");
+
+	cout << "Primeiro nodo contem: " << lista1.readFirst() << endl;
+
+	lista1.listAll();
+
+
 
 	return 0;
 }
