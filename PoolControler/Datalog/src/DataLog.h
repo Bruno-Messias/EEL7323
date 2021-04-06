06ecfba58cdff73d099c0345f0348ee851b33d17
Bruno Cayres Messias <brunocmessias@hotmail.com>
2021-04-06 14:38:58 -0300

    Update comments

--- PoolControler/Datalog/src/DataLog.h
+++ PoolControler/Datalog/src/DataLog.h
@@ -17,5 +17,3 @@ public:
     void conversionLog(string read_log);
     void addLog(int id, string year_s, string month_s, string day_s, string hour_s, string min_s, string sec_s, char event);
 };
-
-    // add close(serial_port);
